8792a38fd61ed2005be0dddf55ce244635d8ab6d
FrancescoGuggino <landongu@gmail.com>
2023-07-10 12:01:53 +0200

    errori

--- js/main.js
+++ js/main.js
@@ -14,3 +14,60 @@ Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto 
 Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto il bottone send.
 */
 
+let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+
+
+
+
+
+//calcolo offerta e sconti
+function offer (event){
+    event.preventDefault();
+
+    console.log(event);
+    
+
+    //prendo dati iseriti dall'utente
+    let hours = parseInt(document.getElementById("hours").value);
+    let optionsOffer = parseInt(document.getElementById("work").value);
+    let discountCode = document.getElementById("discount").value;
+
+
+let finalPrice = 0
+
+    //opzioni lavoro
+    switch(optionsOffer){
+        case 1:
+            finalPrice = 20.5 * hours;
+            break;
+        case 2:
+            finalPrice = 15.3 * hours;
+            break;
+        case 3:
+            finalPrice = 33.6 * hours;
+    }
+
+  //sconto
+    let DiscountCodeCheck = false;
+    for(let i = 0; i < discountCodes.length; i++){
+        if(discountCodes[i] == discountCode){
+            DiscountCodeCheck = true;
+
+            break;
+        }
+    }
+
+    if (!discountCode) {
+        alert("Il codice inserito non è valido!");
+
+    }
+
+    if(DiscountCodePresent){
+        finalPrice = 0.75 * finalPrice;
+    }
+
+    finalPrice = finalPrice.toFixed(2);
+
+    document.getElementById("price").innerHTML = "Il prezzo stimato è di: " + finalPrice;
+
+}
